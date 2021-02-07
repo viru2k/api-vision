75b988ad35fa6cfb948ce0c24bd144a1e0220ac9
Gastón Alejandro Villafañe <gastonvillafane@gmail.com>
2021-02-07 06:54:13 -0300

    minor changes

---
+++ app/Http/Controllers/Liquidacion/FacturaTipoIvaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Liquidacion;
+
+use App\models\FacturaTipoIva;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FacturaTipoIvaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\models\FacturaTipoIva  $facturaTipoIva
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FacturaTipoIva $facturaTipoIva)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\models\FacturaTipoIva  $facturaTipoIva
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FacturaTipoIva $facturaTipoIva)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\models\FacturaTipoIva  $facturaTipoIva
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FacturaTipoIva $facturaTipoIva)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\models\FacturaTipoIva  $facturaTipoIva
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FacturaTipoIva $facturaTipoIva)
+    {
+        //
+    }
+}
